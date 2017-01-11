c12e9091d45abce02de1db4c58ac0e3ed1d68f66
Jeremy <acnajcadcac.yahoo.com>
2017-01-10 21:13:46 -0600

    Added checking to see if the first letter is a number and to make sure there are no special characters

--- CamelCase.py
+++ CamelCase.py
@@ -3,11 +3,27 @@ Author: Jeremy Belisle
 Last Date Modified: 1/10/17
 This program will let the user input a sentence and will convert it to camel case'''
 
-print("Camel Case Converter", "\n")
+print("CamelCase Converter", "\n")
 
-userInput = input("Enter your sentence here: ")  # getting the user input
-userInput = userInput.title()  # using title to convert every word to start with upper case and the rest to be lower case
-userInput = userInput.replace(" ", "")  # getting rid of the spaces
+check = True
 
-print(userInput[0].lower() + userInput[1:])  # Printing the output with the first letter in lower case
+while check:
+
+    userInput = input("Enter your sentence here: ")  # getting the user input
+
+    if userInput[0].isdigit():  # using the isdigit to check and see if the first letter is a number
+        print("Sentence can't start with a number")
+
+    elif userInput[0] != int:  # just checking a few invalid characters
+        if "#" in userInput:
+            print("Sentence can't contain invalid characters #, \", +")
+        elif "\"" in userInput:
+            print("Sentence can't contain invalid characters #, \", +")
+        elif "+" in userInput:
+            print("Sentence can't contain invalid characters #, \", +")
+        else:
+            userInput = userInput.title()  # using title to convert every word to start with upper case and the rest to be lower case
+            userInput = userInput.replace(" ", "")  # getting rid of the spaces
+            check = False
+            print(userInput[0].lower() + userInput[1:])  # Printing the output with the first letter in lower case
 
